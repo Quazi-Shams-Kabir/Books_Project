b611b408e1b9582219a0191db2054b5a0bbdcb8e
Quazi Shams Kabir <quaziraj12@gmail.com>
2024-05-29 02:29:48 +0530

    new db

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const app = express();
 const port = 3000;
 
 // DB
-// for my local machine
-// const db = new pg.Client({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "books",
-//   password: "123456",
-//   port: 5432,
-// });
+ //for my local machine
+ //const db = new pg.Client({
+ //  user: "postgres",
+ //  host: "localhost",
+ //  database: "books",
+ //  password: "12345678",
+ //  port: 5432,
+ //});
 
 // for deployment
 const db = new pg.Client({
   user: "books_project_user",
   password: process.env.PG_PASS,
-  database: "books_project",
+  database: "books_project_89cr",
   port: 5432,
-  host: "dpg-cmdo65f109ks7399mljg-a.singapore-postgres.render.com",
+  host: "dpg-cpb1juv79t8c73b744a0-a.singapore-postgres.render.com",
   ssl:true
 });
 
@@ -49,7 +49,7 @@ async function bookDetails(sort_by, sort_order) {
 let sort_by = "name";
 let sort_order = "ASC";
 
-app.get("/", async (req, res) => {
+app.get("/", async (_, res) => {
   const bookData = await bookDetails(sort_by, sort_order);
   try {
     res.render("index.ejs", {
@@ -136,3 +136,5 @@ VALUES (
 app.listen(port, (req, res) => {
   console.log(`server running on port ${port}`);
 });
+
+// TODO: some sort of waiting sign when searching for books
